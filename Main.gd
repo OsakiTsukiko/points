be9d15fe1f838773c9909373d8cc9456deb8c5f5
OsakiTsukiko <osakitsukiko.mail@gmail.com>
2022-05-03 20:24:37 +0300

    Vector3?

--- Main.gd
+++ Main.gd
@@ -9,8 +9,6 @@ onready var enter_btn = $CanvasLayer/Enter
 onready var cubes = $Cubes
 
 const SPACE = 5
-var squares = []
-var index = 0
 
 func _ready():
 	logger.text = "[Initialized..]\n"
@@ -23,7 +21,7 @@ func _ready():
 #				squares.push_back(instance)
 				
 				var mat = SpatialMaterial.new()
-				var color = Color(0,0,0,1)
+				var color = Color(randf(),randf(),randf(),1)
 				mat.albedo_color = color
 				instance.material_override = mat
 		
@@ -33,18 +31,32 @@ func _ready():
 	pass
 
 func _physics_process(delta):
-	if ( Input.is_action_pressed("ui_accept") ):
+	# Rotate
+	if ( Input.is_action_pressed("ui_right") ):
+		cubes.rotate(Vector3( 0, 1, 0 ), PI/100)
+	if ( Input.is_action_pressed("ui_left") ):
+		cubes.rotate(Vector3( 0, -1, 0 ), PI/100)
+	if ( Input.is_action_pressed("ui_up") ):
+		cubes.rotate(Vector3( -1, 0, 0 ), PI/100)
+	if ( Input.is_action_pressed("ui_down") ):
+		cubes.rotate(Vector3( 1, 0, 0 ), PI/100)
+		
+	# Explode?
+	if ( Input.is_action_pressed("cubes_1") ):
 		for i in cubes.get_children():
-			i.translate(Vector3(i.translation[0] + 1, i.translation[1] + 1, i.translation[2] + 1))
-#			i.translate(Vector3(x * SPACE, y * SPACE, z * SPACE))
+			i.translate(Vector3(i.translation[0], 0, 0))
+	if ( Input.is_action_pressed("cubes_2") ):
+		for i in cubes.get_children():
+			i.translate(Vector3(-i.translation[0], 0, 0))
 	pass
 
 func _enter_btn_pressed():
-	var coords = {
-		"x": int(x_inp.text),
-		"y": int(y_inp.text),
-		"z": int(z_inp.text)
-	}
+	var coords = Vector3(
+		int(x_inp.text),
+		int(y_inp.text),
+		int(z_inp.text)
+	)
+	
 	logger.text += "[" + String(coords.x) + ", " + String(coords.y) + ", " + String(coords.z) + "]\n"
 	var instance = point_scene.instance()
 	instance.translate(Vector3(coords.x, coords.y, coords.z))
